3ce0ae2c43d7ffeeb8197e558bb1b888cfc56c95
badstreff <adam.furbee@gmail.com>
2024-09-20 10:51:48 -0500

    iwp

--- projects/11/compiler.py
+++ projects/11/compiler.py
@@ -18,24 +18,22 @@ def main():
             if os.path.isfile(os.path.join(args.input, file)) and file[-5:] == ".jack"
         ]
         for file in src:
-            print(file)
-            with open(file, "r") as f:
-                tokenizer = JackTokenizer(f)
-                ce = CompilationEngine(tokenizer)
-                result = ce.compile_class()
-                p = pathlib.Path(file)
-                p = p.with_suffix(".xml")
-                with open(p, "w") as out:
-                    tree = ET.ElementTree(result)
-                    tree.write(out, encoding="unicode")
+            compile(file)
     else:
-        with open(args.input, "r") as f:
-            tokenizer = JackTokenizer(f)
-            p = pathlib.Path(args.input)
-            p = p.with_suffix(".vm")
-            with VMWriter(p) as vmwriter:
-                ce = CompilationEngine(tokenizer, vmwriter)
-                ce.compile_class()
+        compile(args.input)
+
+def compile(path: str):
+    '''
+    Takes a single .jack file as a path paramenter and outputs a compiled .vm file in the same directory
+    path: path to the .jack file to be compiled
+    '''
+    with open(path, "r") as f:
+        tokenizer = JackTokenizer(f)
+        p = pathlib.Path(path)
+        p = p.with_suffix(".vm")
+        with VMWriter(p) as vmwriter:
+            ce = CompilationEngine(tokenizer, vmwriter)
+            ce.compile_class()
 
 
 if __name__ == "__main__":
